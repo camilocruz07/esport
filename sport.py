467a66cacbf19353caa7afdf183a70be184c874f
zharick <zhakmar2@gmail.com>
2021-08-25 12:51:56 -0500

    se sube el repositorio a git

---
+++ sport.py
@@ -0,0 +1,20 @@
+numero1= 45
+numero2= 34
+
+
+operacion = input("elija una opcion ")
+if operacion =="a": 
+    suma = numero1 + numero2 
+    print ("el resultado es: ", suma)
+elif operacion =="b":
+    suma = numero1 + numero2
+    print ("el resultado es: ", suma)
+elif operacion =="c":
+    suma = numero1 + numero2
+    print ("el resultado es: ", suma)
+elif operacion =="d":
+    suma = numero1 + numero2
+    print ("el resultado es: ", suma)
+elif operacion =="e":
+    suma = numero1 + numero2
+    print ("el resultado es: ", suma)
